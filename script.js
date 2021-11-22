89542bcf0a9838eef28b387e92b14b65af91f872
Abiodun Segun <84022331+realshegz-github@users.noreply.github.com>
2021-11-22 03:12:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// window.onscroll = function () {
+// 	myFunction();
+// };
+// const header = document.querySelector('header');
+// var sticky = header.offsetTop;
+
+// function myFunction() {
+// 	if (window.pageYOffset >= sticky) {
+// 		header.classList.add('sticky');
+// 	} else {
+// 		header.classList.remove('sticky');
+// 	}
+// }
+// window.onscroll = function () {
+// 	myFunction();
+// };
